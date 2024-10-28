fdc29f557762b8d9cfdcdfa42ca46222e86c7d6e
manuelaguilar5219 <manuelaguilar5219@my.whccd.edu>
2024-10-27 21:24:58 -0700

    wrote code that displays #'s in a triangle using loop

--- script1.js
+++ script1.js
@@ -7,3 +7,18 @@
 // ###
 // ##
 // #
+function printTriangle() {
+    let n = 4; // Height of the triangle
+
+    // Ascending part of the triangle
+    for (let i = 1; i <= n; i++) {
+        console.log("#".repeat(i));
+    }
+
+    // Descending part of the triangle
+    for (let i = n - 1; i > 0; i--) {
+        console.log("#".repeat(i));
+    }
+}
+
+printTriangle();
